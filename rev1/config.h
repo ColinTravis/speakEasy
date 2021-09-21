90c95d1a258de599bbb1a03a7f65672f0941d0f8
ColinTravis <colint@cybertravis.us>
2021-09-21 00:14:06 -0500

    Whoops, forgot an LED

--- rev1/config.h
+++ rev1/config.h
@@ -38,7 +38,7 @@
 
 #define RGB_DI_PIN D3
 #ifdef RGB_DI_PIN
-  #define RGBLED_NUM 17
+  #define RGBLED_NUM 18
   #define RGBLIGHT_HUE_STEP 8
   #define RGBLIGHT_SAT_STEP 8
   #define RGBLIGHT_VAL_STEP 8
